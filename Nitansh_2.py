20b12d49ba067b4e11b467dc6e1d4ed026ed13c8
Nitansh <nitansh.bareja@gmail.com>
2014-07-15 18:55:39 +0530

    Comments updated

--- Nitansh_2.py
+++ Nitansh_2.py
@@ -6,23 +6,27 @@ class CompanyInfo(object):
     """ The company info is stored in it """  
   
     def __init__(self, company_name):
+      """called when object isntance is created"""
       self.__company_price__list = []
       self.__company_time_list   = []
       self.__name = company_name
 
     def add_info(self, year, month, price):
+      """ add the company info to each list"""
       self.__company_time_list.append(year + ' ' + month) 
       self.__company_price__list.append(price)  
 
     def find_peak_share_time(self):
+       """find the peak time when the share was at the highest peak position """
        indices = [i for i, x in enumerate(self.__company_price__list) if x == max(self.__company_price__list)]
        time_string = ''
        for index in indices:
-          time_string += self.__company_time_list[index] + ' '
+          time_string += self.__company_time_list[index] + ' ' # appending all the time strings when the rate was at peak position    
 
        return time_string, self.__company_price__list[indices[0]] 
 
     def share_info(self):
+      """ return a formatted form of the final output"""
       return self.__name + ' ---> the peak time is %s and Share at that time is %s '%  self.find_peak_share_time()  
 
 class MainHandler(object):
@@ -33,7 +37,7 @@ class MainHandler(object):
 
   def final_info(self):
     for company in self.company_list:
-      print company.share_info()
+      print company.share_info()                   # printing the final output
       
   def add_company(self, company):
     self.company_list.append(company)
@@ -44,19 +48,19 @@ class MainHandler(object):
     month = row[1]
     share_prices = row[2:]
     for x in range(0, len(self.company_list)):
-      self.company_list[x].add_info(year, month, share_prices[x])
+      self.company_list[x].add_info(year, month, share_prices[x])  # populating the company info (year month and share_price)
 
   def read_from_file(self,filename):
     """ Read the given csv file """
-    count = 0  
-    with open(filename, 'rb') as file_object:
+
+    with open(filename, 'rb') as file_object:        
       csv_reader = reader(file_object)
-      companies_name = next(csv_reader)
+      companies_name = next(csv_reader)             # fetching the header first 
       for company_name in companies_name[2:]:
-        self.add_company(CompanyInfo(company_name)) 
+        self.add_company(CompanyInfo(company_name)) # making companyinfo object for each company
       try :
         while(True):
-          self.process_row(next(csv_reader))
+          self.process_row(next(csv_reader))        # processing the data
       except:
         pass # stoping the iteration
       file_object.close()
@@ -64,12 +68,15 @@ class MainHandler(object):
 
 if __name__ == "__main__":
   
-  if not sys.argv[1]:
+  # check whether the filename is given or not
+  if len(sys.argv) != 2 :
     print 'Please enter the filename'
+    sys.exit()
   
+  # check whether the file exists or not
   if os.path.exists(sys.argv[1]):
-    my_main_handler = MainHandler()
-    my_main_handler.read_from_file(sys.argv[1])
-    my_main_handler.final_info()
+    my_main_handler = MainHandler()                 # making an instance of Mainhandler
+    my_main_handler.read_from_file(sys.argv[1])     # calling the readfile function
+    my_main_handler.final_info()                    # printing the final info to the console
   else :
     print 'Please check the file is present or not'
